7f7379f7ca967e1324dcf3dac2abf6d049bc43b3
ayush-lal <ayush.lal@outlook.com>
2022-12-12 22:34:47 +1000

    initial commit of InterfaceVsInheritance demo

---
+++ InterfaceVsInheritance/InterfaceVsInheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InterfaceVsInheritance
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+}
